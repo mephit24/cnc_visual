15b0f130cd09043f2f12b28cb160298b9cbbb80a
mephit <meph.cell@gmail.com>
2023-04-25 21:25:03 +0700

    Add coords sum feature

--- gui.py
+++ gui.py
@@ -14,12 +14,17 @@ window.title('CNC_visual')
 window.geometry('50x150+1500-800')
 
 btn_redraw = tkinter.Button(window, text='REDRAW',
-                     command=lambda: main.visualize(main.cleared_frames_arr(main.get_frames_arr_from_file('myfile.txt'))))
+                     command=lambda: main.visualize(main.cleared_frames_arr(main.get_frames_arr_from_file(path))))
 btn_redraw.grid(column=0)
 btn_redraw.focus()
 btn_redraw.pack(fill='x')
 
 btn_exit = tkinter.Button(window, text='Exit', command=sys.exit)
 btn_exit.pack(anchor='s', expand=True)
+
+coord_sum = str(main.check_sum_coords(main.cleared_frames_arr(main.get_frames_arr_from_file(path))))
+output = tkinter.Text()
+output.insert('1.0', coord_sum)
+output.pack()
 
 window.mainloop()
--- main.py
+++ main.py
@@ -43,13 +43,16 @@ def cleared_frames_arr(dirty_frames_arr: list) -> tuple[ClearFrame]:
     return tuple(cleared_arr)
 
 
-def check_sum_coords(cleared_frames_arr: tuple) -> bool: # TODO
-    pass
+def check_sum_coords(cleared_frames_arr):
+    xgen = (coor.xyz()[0] for coor in cleared_frames_arr)
+    ygen = (coor.xyz()[1] for coor in cleared_frames_arr)
+    zgen = (coor.xyz()[2] for coor in cleared_frames_arr)
+    return (sum(xgen), sum(ygen), sum(zgen))
 
 
 def visualize(cleared_frames_arr: tuple[ClearFrame]):
     scr = turtle.Screen()
-    scr.setup(1280, 800, startx=0, starty=0)
+    scr.setup(1280, 900, startx=0, starty=0)
     scr.clear()
 
     xy_cur = turtle.Turtle()
@@ -58,7 +61,7 @@ def visualize(cleared_frames_arr: tuple[ClearFrame]):
     xy_cur.pensize(2)
     xy_cur.goto(-300, 50)
     xy_cur.pendown()
-    xy_cur.speed(1)
+    xy_cur.speed(3)
 
     xz_cur = turtle.Turtle()
     xz_cur.penup()
@@ -66,18 +69,18 @@ def visualize(cleared_frames_arr: tuple[ClearFrame]):
     xz_cur.pensize(2)
     xz_cur.goto(200, 50)
     xz_cur.pendown()
-    xz_cur.speed(1)
+    xz_cur.speed(3)
 
     txt_cur = turtle.Turtle()
     txt_cur.penup()
     txt_cur.speed(10)
-    txt_cur.setpos(400, 350)
+    txt_cur.setpos(400, 400)
     txt_cur.pendown()
 
     for frame in cleared_frames_arr:
         txt_cur.write(frame)
         txt_cur.penup()
-        txt_cur.goto(txt_cur.pos() + (0, -15))
+        txt_cur.goto(txt_cur.pos() + (0, -12))
         txt_cur.pendown()
         
         xy_cur.setpos(xy_cur.pos() + frame.xy())
@@ -85,7 +88,6 @@ def visualize(cleared_frames_arr: tuple[ClearFrame]):
         time.sleep(DELAY)
 
     turtle.done()
-    
 
 
 if __name__ == '__main__':
